303fe1c138cb9715234a344b0a436216a0f93354
Andres Ojeda <andyojeda2003@gmail.com>
2024-08-31 09:33:03 -0500

    VFinal

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
-// environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://conexia-backend.vercel.app/api', // Cambia esta URL
-  imageUrl: 'https://conexia-backend.vercel.app/uploads/' // Cambia esta URL si tus imágenes se sirven desde esta ruta
+  apiUrl: 'https://conexiabackend-production.up.railway.app/api', // Nueva URL de la API
+  imageUrl: 'https://conexiabackend-production.up.railway.app/uploads/' // Nueva URL para las imágenes (ajusta si es necesario)
 };
